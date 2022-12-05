d02cd8618d7d76160959589e8385dbf4d6f89f28
Ryguy <ryan70297@gmail.com>
2022-12-05 18:43:04 -0400

    added text to speech , search history , and second news api

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
-
-
+ 
 function getTrendingNews(option) {
     $("#trending").empty();
-    var apiUrl = "https://newsdata.io/api/1/news?apikey=pub_14107a7481f5c321a6b3ae7415b27d544ad0b&country=ca,us&language=en&category=" + option;
-    //  + option;
-    
+   
+    var apiUrl = "https://newsdata.io/api/1/news?apikey=pub_1412872da2511db6023cd470fd39cc0b01a8b&&country=ca,us&category=" + option;
+
 
     fetch(apiUrl)
         .then(function (response) {
@@ -15,6 +14,11 @@ function getTrendingNews(option) {
                     console.log(data);
 
                     for (let index = 0; index < data.results.length; index++) {
+
+                        // $("a").on("click", function () {
+                        //     Window.open(link._blank)
+                        // });
+ 
 
                         var title = data.results[index].title;
                         var description = data.results[index].description;
@@ -30,7 +34,7 @@ function getTrendingNews(option) {
 
                         else if (imageUrl === null) {
                             $("#trending").append(
-                                 "<h2>" + title + "</h2>"
+                                "<h2>" + title + "</h2>"
                                 + "<br>"
                                 + "<p class='desc'>" + description + "</p>"
                                 + "<a href='" + link + "'>"
@@ -38,11 +42,12 @@ function getTrendingNews(option) {
                                 + "Read More"
                                 + "</button>"
                                 + "</a>"
+                                + "<button class='saveBtn1'>" + "Save" + "</button>"
                                 + "<hr>"
                             )
                         } else if (description === null) {
                             $("#trending").append(
-                                 "<h2>" + title + "</h2>"
+                                "<h2>" + title + "</h2>"
                                 + "<br>"
                                 + "<img src='" + imageUrl + "'/>"
                                 + "<a href='" + link + "'>"
@@ -50,16 +55,17 @@ function getTrendingNews(option) {
                                 + "Read More"
                                 + "</button>"
                                 + "</a>"
+                                + "<button class='saveBtn1'" + "Save" + "</button>"
                                 + "<hr>"
                             )
                         }
                         else if (imageUrl !== null && imageUrl.includes(".mp4")) {
-                        //    do nothing
-                        console.log("video");
+
+                            console.log("video");
                         }
-                         else {
+                        else {
                             $("#trending").append(
-                                 "<h2>" + title + "</h2>"
+                                "<h2>" + title + "</h2>"
                                 + "<br>"
                                 + "<img src='" + imageUrl + "'/>"
                                 + "<br>"
@@ -69,6 +75,7 @@ function getTrendingNews(option) {
                                 + "Read More"
                                 + "</button>"
                                 + "</a>"
+                                + "<button class='saveBtn1'>" + "Save" + "</button>"
                                 + "<hr>"
 
                             )
@@ -84,115 +91,127 @@ function getTrendingNews(option) {
 getTrendingNews(option = "top");
 
 
-
 function getSearchedNews(search) {
-    $("#searchednews").empty();
-    var apiUrl = "https://newsdata.io/api/1/news?apikey=pub_14107a7481f5c321a6b3ae7415b27d544ad0b&from_date=2022-11-1&to_date=2022-12-1&country=ca,us&language=en&q=" + search.replace(" ", "%20");
-
-
-    fetch(apiUrl)
-        .then(function (response) {
-            if (response.ok) {
-                console.log(response);
-                response.json().then(function (data) {
-                    console.log(data);
-
-
-                    if(data.totalResults === 0){
-                        $("#searchednews").append(
-                            "<h2>No results found</h2>"
-                        )
-                    }
-
-                    for (let index = 0; index < data.results.length; index++) {
-
-                        $("a").on("click", function () {
-                            Window.open(link)
-                        });
-
-                        var title = data.results[index].title;
-                        var description = data.results[index].description;
-                        var link = data.results[index].link;
-                        var imageUrl = data.results[index].image_url;
-
-                        console.log(data.results[index].image_url);
-
-                        
-                        if (imageUrl === null && description === null) {
-                            console.log("nothing");
-                        }
-
-                        else if (imageUrl === null) {
-                            $("#searchednews").append(
-                                 "<h2>" + title + "</h2>"
-                                + "<br>"
-                                + "<p class='desc'>" + description + "</p>"
-                                + "<a href='" + link + "'>"
-                                + "<button>"
-                                + "Read More"
-                                + "</button>"
-                                + "</a>"
-                                + "<hr>"
-                            )
-                        } else if (description === null) {
-                            $("#searchednews").append(
-                                 "<h2>" + title + "</h2>"
-                                + "<br>"
-                                + "<img src='" + imageUrl + "'/>"
-                                + "<a href='" + link + "'>"
-                                + "<button>"
-                                + "Read More"
-                                + "</button>"
-                                + "</a>"
-                                + "<hr>"
-                            )
-                        }
-                        else if (imageUrl !== null && imageUrl.includes(".mp4")) {
-                       
-                        console.log("video");
-                        }
-                         else {
-                            $("#searchednews").append(
-                                 "<h2>" + title + "</h2>"
-                                + "<br>"
-                                + "<img src='" + imageUrl + "'/>"
-                                + "<br>"
-                                + "<p class='desc'>" + description + "</p>"
-                                + "<a href='" + link + "'>"
-                                + "<button>"
-                                + "Read More"
-                                + "</button>"
-                                + "</a>"
-                                + "<hr>"
-
-                            )
-
-                        }
-
-                    }
-                });
-            }
-
-
+    $("#searchednews").empty()
+  
+   
+      const options = {
+          method: 'GET',
+          headers: {
+              'X-RapidAPI-Key': '30e2c5b198msh98fd7763e5904f2p163deajsn67969615a5a2',
+              'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+          }
+      };
+      
+      fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q='+ search.replace(" ", "%20") + '&pageNumber=1&pageSize=20&autoCorrect=true&safeSearch=true&withThumbnails=true&fromPublishedDate=null&toPublishedDate=null', options)
+      .then(function (response) {
+          if (response.ok) {
+              console.log(response);
+              response.json().then(function (data) {
+                  console.log(data);
+  
+      
+                  if (data.totalCount === 0) {
+                      $("#searchednews").append(
+                          "<h2>No results found</h2>"
+                          + "<img src='https://dlvkyia8i4zmz.cloudfront.net/vEQWzpo0QyNT5Bgmy8oc_2020_06_03.gif'/>"
+                      )
+                  }
+  
+  
+  for (let index = 0; index < data.value.length; index++) {
+      
+      var title = data.value[index].title;  
+      var description = data.value[index].description;
+      var link = data.value[index].url;
+      var imageUrl = data.value[index].image.url;
+  
+      $("#searchednews").append(
+          "<h2>" + title + "</h2>"
+          + "<br>"
+          + "<img src='" + imageUrl + "'/>"
+          + "<br>"
+          + "<p class='desc'>" + description + "</p>"
+          + "<a href='" + link + "'>"
+          + "<button>"
+          + "Read More"
+          + "</button>"
+          + "</a>"
+          + "<button class='saveBtn2'>" + "Save" + "</button>"
+          + "<hr>"
+  
+      )
+  }
+  
+              });
+          }
+      })
+      
+  
+  }
         
 
-        });
-}
 
 $("#trendingSel").on("change", function () {
-    option = (this.value);
+    option = $(this).val();
     getTrendingNews(option);
 })
 
-$("#searchbtn").on("click", function () {  
-  
+$("#searchbtn").on("click", function () {
+if ($("#searchbar").val() !== " " ) {
     search = $("#searchbar").val();
     getSearchedNews(search);
-});
+    searchHistory = []
+    var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
+    searchHistory.push(search);
+    localStorage.setItem("history", JSON.stringify(searchHistory));
+}
+}) 
+
+$("#search-history").on("click", function () {
+    
+getHistory();
+}
+)
+//  make save news fuction  and save to local storage
+
+
+function getHistory() {
+
+    var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
+for (let i = 0; i < searchHistory.length; i++) {
+    
+   
+    $("#historyDiv").append(
+       "<a  class='history' data='" + searchHistory[i] + "'>" + searchHistory[i] + "</a>"
+        + "<br>");
+}
+$(".history").on("click", function () {
+    $("#searchbar").val($(this).attr("data"))
+      
+  })
+
+}
+
+$(".saveBtn").on("click", function () {
+
+})
+
+
+var SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+recognition.onresult = function (event) {
+    var text = event.results[0][0].transcript;
+    $("#searchbar").val(text);
+}
+
+$("#speech").on("click", function () {
+    recognition.start();
+}
+)
 
 
 
+console.log(recognition);
 
-// var option = $("#trendingSelect").find(":selected").val();
-
-// console.log($("option").getAttribute("value"));
